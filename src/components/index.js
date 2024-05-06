5b7fff57ee3de55aae2b9234f0ea29b24c003a83
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-05-06 11:09:19 +0200

    Impove imports for components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from 'components/App.jsx';
+export * from 'components/Searchbar/Searchbar.jsx';
+export * from 'components/ImageGallery/ImageGallery.jsx';
+export * from 'components/ImageGalleryItem/ImageGalleryItem.jsx';
+export * from 'components/Loader/Loader.jsx';
+export * from 'components/Button/Button.jsx';
+export * from 'components/Modal/Modal.jsx';
