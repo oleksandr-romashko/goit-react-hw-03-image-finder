f99bf22bf07f1ba826963ce4362bec83402f65c5
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-05-09 12:36:05 +0200

    Improve ux search

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,33 +1,40 @@
+import React from "react";
 import css from "./SearchForm.module.css";
-
-
 
 /**
  * Form to submit search query.
  * @returns {React.Component}
  */
-export const SearchForm = ({ submit }) => {
-  const handleSubmit = (event) => {
+export class SearchForm extends React.Component {
+
+  componentDidMount() { 
+    const input = document.getElementById("query");
+    input.focus();
+   }
+
+  handleSubmit = (event) => {
     event.preventDefault();
     const searchQuery = event.target.query.value;
-      submit(searchQuery);
+      this.props.submit(searchQuery);
   }
   
-  return (
-    <form className={css.form} onSubmit={handleSubmit}>
+  render() {
+    return (
+    <form className={css.form} onSubmit={this.handleSubmit}>
       <button type="submit" className={css.button}>
         <span className={css['button-label']}>Search</span>
       </button>
       
-      <input
-        className={css.input}
-        name="query"
-        type="text"
-        autoComplete="off"
-        autoFocus
-        placeholder="Search images and photos"
-        required
-      />
+        <input
+          id="query"
+          className={css.input}
+          name="query"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          required
+       />
     </form>
-  )
-};
+  )}
+}
