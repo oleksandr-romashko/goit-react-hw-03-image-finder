b4e65d11abd3c79b28f7c9e35f76d998a6700217
Oleksandr Romashko <alex.rmshk@gmail.com>
2024-05-10 14:26:35 +0200

    Add axios error handling timeout

--- src/api/images/index.js
+++ src/api/images/index.js
@@ -1,5 +1,10 @@
 import { instance } from 'api/instance';
 
+const ERROR_MESSAGE_DEFAULT =
+  'Application internal error. Please contact application administrator.';
+const ERROR_MESSAGE_TIMEOUT =
+  'Sorry, but your request was canceled due to a long time waiting for a response from the server.';
+
 export const getImagesApi = async (searchQuery, page, perPage) => {
   const { data } = await instance(`?q=${searchQuery}`, {
     params: {
@@ -7,6 +12,16 @@ export const getImagesApi = async (searchQuery, page, perPage) => {
       per_page: perPage,
       image_type: 'photo',
     },
+    signal: AbortSignal.timeout(5000), //https://axios-http.com/docs/cancellation
+  }).catch(function (error) {
+    console.log(typeof error);
+    console.log(error);
+    if (error.code === 'ERR_CANCELED') {
+      throw new Error(ERROR_MESSAGE_TIMEOUT);
+    } else {
+      const message = `${ERROR_MESSAGE_DEFAULT} Reason: '${error.message}'.`;
+      throw new Error(message);
+    }
   });
   return data;
 };
